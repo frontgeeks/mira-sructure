6bc4406d0b19de271aa627752b89e731e177ad8e
Dilan Mckey <mckey@mini.local>
2014-04-28 12:59:49 +0600

    clean

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Fast app structure creator
 
-
+## Usage
+  var miraStructure = require('mira-structure');
+  miraStructure.getTemplate(TemplateName, AppName)
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
+"use strict";
 var miraStructure = require('./lib/mira-structure');
 module.exports = miraStructure;
-//  template
-//  miraStructure.getTemplate(TemplateName, AppName)
